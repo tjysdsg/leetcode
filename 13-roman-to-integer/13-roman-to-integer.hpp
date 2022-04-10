68ac54cdf315472c3833c52818954f9ba542cd51
tjysdsg <tjy1018543509@163.com>
2022-04-10 16:26:55 +0800

    13 roman to integer unoptimized

--- 13-roman-to-integer/13-roman-to-integer.hpp
+++ 13-roman-to-integer/13-roman-to-integer.hpp
@@ -1,12 +1,68 @@
+// https://leetcode-cn.com/problems/roman-to-integer/
+
 #pragma once
 #include <string>
 #include <iostream>
 using std::string;
 using std::cout;
 
+inline int normal_conversion(char c) {
+  int ret = 0;
+  switch (c) {
+    case 'I':
+      ret = 1;
+      break;
+    case 'V':
+      ret = 5;
+      break;
+    case 'X':
+      ret = 10;
+      break;
+    case 'L':
+      ret = 50;
+      break;
+    case 'C':
+      ret = 100;
+      break;
+    case 'D':
+      ret = 500;
+      break;
+    case 'M':
+      ret = 1000;
+      break;
+    default:
+      break;
+  }
+  return ret;
+}
+
 class Solution {
  public:
   int romanToInt(string s) {
-    return 3;
+    int res = 0;
+
+    int n = s.length();
+    int i = 0;
+    while (i < n) {
+      int curr = -1;
+
+      if (i < n - 1) { // check subtraction
+        if (s[i] == 'I' && s[i + 1] == 'V') curr = 4;
+        else if (s[i] == 'I' && s[i + 1] == 'X') curr = 9;
+        else if (s[i] == 'X' && s[i + 1] == 'L') curr = 40;
+        else if (s[i] == 'X' && s[i + 1] == 'C') curr = 90;
+        else if (s[i] == 'C' && s[i + 1] == 'D') curr = 400;
+        else if (s[i] == 'C' && s[i + 1] == 'M') curr = 900;
+      }
+
+      if (curr == -1) // no subtraction
+        curr = normal_conversion(s[i]);
+      else // skip 2 when there is subtraction
+        ++i;
+
+      ++i;
+      res += curr;
+    }
+    return res;
   }
 };
--- 13-roman-to-integer/test.h
+++ 13-roman-to-integer/test.h
@@ -2,10 +2,23 @@
 #include "13-roman-to-integer.hpp"
 #include <gtest/gtest.h>
 
-TEST(roman_to_integer, all) {
+TEST(roman_to_integer, 1) {
   string in = "III";
   int out = 3;
   Solution sol;
+  EXPECT_EQ(sol.romanToInt(in), out);
+}
 
+TEST(roman_to_integer, 2) {
+  string in = "LVIII";
+  int out = 58;
+  Solution sol;
+  EXPECT_EQ(sol.romanToInt(in), out);
+}
+
+TEST(roman_to_integer, 3) {
+  string in = "MCMXCIV";
+  int out = 1994;
+  Solution sol;
   EXPECT_EQ(sol.romanToInt(in), out);
 }
