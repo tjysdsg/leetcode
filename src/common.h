90e6d4f5506cdb3c810b176b691b4d53d2370f13
tjysdsg <tjy1018543509@163.com>
2022-09-03 21:10:56 -0400

    Update template, simplify includes

--- src/common.h
+++ src/common.h
@@ -1,6 +1,29 @@
 #pragma once
 
+#include <string>
+#include <iostream>
+#include <algorithm>
 #include <vector>
+#include <cstring>
+#include <map>
+#include <set>
+#include <stack>
+#include <cassert>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+
+using std::string;
+using std::cout;
+using std::vector;
+using std::map;
+using std::unordered_map;
+using std::unordered_set;
+using std::set;
+using std::stack;
+using std::queue;
+using std::priority_queue;
 
 struct TreeNode {
     int val;
--- src/template/sol.hpp
+++ src/template/sol.hpp
@@ -2,30 +2,7 @@
 
 #pragma once
 
-#include <string>
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <cstring>
-#include <map>
-#include <set>
-#include <stack>
-#include <cassert>
-#include <cmath>
-#include <unordered_map>
-#include <unordered_set>
-#include <queue>
 #include "common.h"
-
-using std::string;
-using std::cout;
-using std::vector;
-using std::map;
-using std::unordered_map;
-using std::unordered_set;
-using std::set;
-using std::stack;
-using std::queue;
 
 class Solution {
 public:
