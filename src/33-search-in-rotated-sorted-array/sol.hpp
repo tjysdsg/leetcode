4df32301e6110ab7cca4e765deddc057f7088659
tjysdsg <tjy1018543509@163.com>
2023-08-06 12:46:47 -0400

    Review 33

--- src/33-search-in-rotated-sorted-array/sol.hpp
+++ src/33-search-in-rotated-sorted-array/sol.hpp
@@ -2,25 +2,7 @@
 
 #pragma once
 
-#include <string>
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <cstring>
-#include <map>
-#include <set>
-#include <stack>
-#include <cassert>
-#include <cmath>
-#include <unordered_map>
-
-using std::string;
-using std::cout;
-using std::vector;
-using std::map;
-using std::unordered_map;
-using std::set;
-using std::stack;
+#include "common.h"
 
 class Solution {
 public:
